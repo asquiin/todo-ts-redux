5dab909b7b6037871fc3127a85bf769e8fdac579
Assel <assel.amerkanova@nu.edu.kz>
2023-11-03 09:28:33 +0600

    add todo

--- src/Todo/AddTodo.tsx
+++ src/Todo/AddTodo.tsx
@@ -4,24 +4,24 @@ import { bindActionCreators } from "redux";
 import { actionCreators } from "../state";
 import { Button } from "react-bootstrap";
 export const AddTodo = () => {
-    const dispatch = useDispatch();
-    const [newTaskDescription, setNewTaskDescription] = useState("");
-  
-    const { addTask } = bindActionCreators(actionCreators, dispatch);
-  
-    const handleAddTask = () => {
-      if (newTaskDescription.trim() !== "") {
-        addTask({
-          id: 0, 
-          description: newTaskDescription,
-        });
-        setNewTaskDescription("");
-      }
-    };
-  
+  const dispatch = useDispatch();
+  const [newTaskDescription, setNewTaskDescription] = useState("");
+
+  const { addTask } = bindActionCreators(actionCreators, dispatch);
+
+  const handleAddTask = () => {
+    if (newTaskDescription.trim() !== "") {
+      addTask({
+        id: 0,
+        description: newTaskDescription,
+      });
+      setNewTaskDescription("");
+    }
+  };
+
   return (
     <>
-          <div className="d-flex justify-content-center">
+      <div className="d-flex justify-content-center">
         <input
           type="text"
           placeholder="Enter a new task"
